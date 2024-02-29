93626ca6903320f946902d415b4de0bbc2460f71
TangBeginner <136591758+TangBeginner@users.noreply.github.com>
2024-03-01 00:04:12 +0800

    Add files via upload

---
+++ services/ServiceInterface.java
@@ -0,0 +1,15 @@
+package Services;
+
+// Abstract Class
+public interface ServiceInterface {
+
+    // Abstract Methods
+    void addBorrowedBook();
+
+    void showAllBorrowedBook();
+
+    void returnBook();
+
+    void renewDate();
+
+}
